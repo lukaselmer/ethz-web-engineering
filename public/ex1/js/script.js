7bf5712356fc7e9defc5a3efff640825defb5559
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-18 20:56:48 +0100

    thumbnail js

--- public/ex1/js/script.js
+++ public/ex1/js/script.js
@@ -11,25 +11,18 @@
 
 
 $(document).ready(function () {
-  function PanoramaWidget() {
-    var widget = this;
-    var slider = $(".image-slider");
-    var cropped = slider.children(".cropped");
-    var thumbnail = slider.children(".thumbnail");
-    var thumbnailImg = thumbnail.children("img");
-    var rectangle = thumbnail.children(".rectangle");
+  function BigImage(_cropped) {
+    var cropped = _cropped,
+      croppedImg = cropped.children("img"),
+      lastPageX = 0,
+      lastPageY = 0;
 
-    var img = cropped.children("img");
-    var lastPageX = 0, lastPageY = 0, firstDrag = true;
-    var autodragEnabled = true, autoMoveRight = true;
+    this.initDrag = function (pageX, pageY) {
+      lastPageX = pageX;
+      lastPageY = pageY;
+    }
 
     this.drag = function (event) {
-      if (firstDrag) {
-        lastPageX = event.pageX;
-        lastPageY = event.pageY;
-        firstDrag = false;
-      }
-
       var diffX = lastPageX - event.pageX;
       var diffY = lastPageY - event.pageY;
 
@@ -41,17 +34,17 @@ $(document).ready(function () {
     }
 
     this.moveX = function (delta) {
-      var max = this.maxWidth();
+      var max = this.maxMarginLeft();
       this.move("margin-left", delta, max);
     }
 
     this.moveY = function (delta) {
-      var max = this.maxHeight();
+      var max = this.maxMarginTop();
       this.move("margin-top", delta, max);
     }
 
     this.move = function (direction, delta, max) {
-      var mt = -parseInt(img.css(direction));
+      var mt = -parseFloat(croppedImg.css(direction));
       var position = mt + delta;
       this.moveTo(direction, position, max);
     }
@@ -59,12 +52,61 @@ $(document).ready(function () {
     this.moveTo = function (direction, position, max) {
       if (position < 0) position = 0;
       if (position > max) position = max;
-      img.css(direction, -position + "px");
+      croppedImg.css(direction, -position + "px");
     }
+
+    this.maxMarginTop = function () {
+      return croppedImg.height() - cropped.height();
+    }
+
+    this.maxMarginLeft = function () {
+      return croppedImg.width() - cropped.width();
+    }
+
+    this.widthVisibleRatio = function () {
+      return cropped.width() / croppedImg.width();
+    }
+
+    this.marginLeftRatio = function () {
+      return parseFloat(croppedImg.css("margin-left")) / croppedImg.width(); //this.maxMarginLeft();
+    }
+  }
+
+  function Thumbnail(_thumbnail) {
+    var thumbnail = _thumbnail;
+    var thumbnailImg = thumbnail.children("img");
+    var rectangle = thumbnail.children(".rectangle");
+
+    this.updateRectangle = function (bigImage) {
+      var widthRatio = bigImage.widthVisibleRatio();
+      rectangle.css("height", parseFloat(thumbnailImg.height()) + "px");
+      rectangle.css("width", (thumbnailImg.width() * widthRatio) + "px");
+
+      var marginLeftRatio = bigImage.marginLeftRatio();
+      rectangle.css("margin-left", -marginLeftRatio * thumbnail.width() + "px")
+    }
+  }
+
+  function PanoramaWidget() {
+    var widget = this;
+    var slider = $(".image-slider");
+    var cropped = slider.children(".cropped");
+
+    var firstDrag = true;
+
+    var autodragEnabled = true;
+
+    var bigImage = new BigImage(cropped);
+    var thumbnail = new Thumbnail(slider.children(".thumbnail"));
 
     this.registerEvents = function () {
       var manualDragBig = function (event) {
-        widget.drag(event);
+        if (firstDrag) {
+          bigImage.initDrag(event.pageX, event.pageY);
+          firstDrag = false;
+        }
+        bigImage.drag(event);
+        thumbnail.updateRectangle(bigImage);
       }
 
       slider.on("dragstart", function () {
@@ -88,22 +130,14 @@ $(document).ready(function () {
       autodragEnabled = false;
     }
 
-    this.maxHeight = function () {
-      return img.height() - cropped.height();
-    }
-
-    this.maxWidth = function () {
-      return img.width() - cropped.width();
-    }
-
     this.autoDrag = function (time) {
-      var max = this.maxWidth();
+      var max = bigImage.maxMarginLeft();
       var speed = 0.1;
       var t = (time * speed) % (max * 2);
       if (t > max) t = 2 * max - t;
       var newPosition = Math.abs(t);
-      this.moveTo("margin-left", newPosition, max);
-      this.updateRectangle();
+      bigImage.moveTo("margin-left", newPosition, max);
+      thumbnail.updateRectangle(bigImage);
     }
 
     this.initAutoDrag = function () {
@@ -115,10 +149,6 @@ $(document).ready(function () {
       requestAnimationFrame(drag);
     }
 
-    this.updateRectangle = function(){
-      rectangle.css("height", parseInt(thumbnailImg.height()) + "px");
-      rectangle.css("width", thumbnailImg.height() + "px");
-    }
   }
 
   var w = new PanoramaWidget();
