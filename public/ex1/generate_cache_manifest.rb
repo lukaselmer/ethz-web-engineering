dd9780f1c658cd38edac9e3db646761d41bf0f1c
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-20 23:54:34 +0100

    enhance manifest

--- public/ex1/generate_cache_manifest.rb
+++ public/ex1/generate_cache_manifest.rb
@@ -3,6 +3,9 @@ File.open('cache.appcache', 'w') do |f|
   f.puts "# v = 1.0.0"
   f.puts 'FALLBACK :'
   f.puts '    / /index.html'
+  f.puts 'index.html'
+  f.puts 'reviewers.html'
+  f.puts 'weekly.html'
   %w(js css images).each do |folder|
     Dir.glob("#{folder}/**/*.*") do |file|
       f.puts "#{file}"
