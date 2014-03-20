9d319aeefeaa0a64f6766f59be4eb155faa15fe5
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-20 23:34:12 +0100

    acceleration / easing out

--- public/ex1/js/script.js
+++ public/ex1/js/script.js
@@ -11,11 +11,45 @@
 
 
 $(document).ready(function () {
+  function Vector(_x, _y) {
+    var x = _x, y = _y;
+
+    this.getX = function () {
+      return x;
+    }
+    this.getY = function () {
+      return y;
+    }
+    this.multiply = function (v) {
+      return new Vector(x * v.getX(), y * v.getY());
+    }
+    this.multiplyScalar = function (scalar) {
+      return new Vector(x * scalar, y * scalar);
+    }
+    this.plus = function (v) {
+      return new Vector(x + v.getX(), y + v.getY());
+    }
+    this.minus = function (v) {
+      return new Vector(x - v.getX(), y - v.getY());
+    }
+    this.abs = function () {
+      return new Vector(Math.abs(x), Math.abs(y));
+    }
+    this.toString = function () {
+      return "" + x + "/" + y;
+    }
+  }
+
   function BigImage(_cropped) {
     var cropped = _cropped,
       croppedImg = cropped.children("img"),
       lastPageX = 0,
-      lastPageY = 0;
+      lastPageY = 0,
+      moveCallback = null;
+
+    this.onMove = function (callback) {
+      moveCallback = callback;
+    }
 
     this.initDrag = function (pageX, pageY) {
       lastPageX = pageX;
@@ -28,6 +62,8 @@ $(document).ready(function () {
 
       this.moveX(diffX);
       this.moveY(diffY);
+
+      if (moveCallback !== null) moveCallback(new Vector(diffX, diffY));
 
       lastPageX = pageX;
       lastPageY = pageY;
@@ -184,6 +220,94 @@ $(document).ready(function () {
     }
   }
 
+  function Record(_time, _diff) {
+    var time = _time, diff = _diff;
+
+    this.getTime = function () {
+      return time;
+    }
+    this.getDiff = function () {
+      return diff;
+    }
+  }
+
+  function Accelerator(_moveImage) {
+    var moveImage = _moveImage;
+    var recording;
+    var records;
+    var maxRecordLength = 100;
+
+    // only movements in the last 500 milliseconds are important
+    var relevantMovementsTimeAgo = 500;
+
+    this.clear = function () {
+      records = [new Record(new Date(), new Vector(0., 0.))];
+      recording = false;
+    };
+
+    this.recordMove = function (movementVector) {
+      if (!recording) return;
+
+      records.push(new Record(new Date(), movementVector));
+      if (records.length > maxRecordLength) records.shift();
+    };
+
+    this.calculateMovement = function () {
+      var d = new Date();
+      var v = new Vector(0, 0);
+      var oldest = d;
+
+      // could make this faster...
+      records.forEach(function (el) {
+        if (d - el.getTime() <= relevantMovementsTimeAgo) {
+          v = v.plus(el.getDiff());
+          if (el.getTime() < oldest) oldest = el.getTime();
+        }
+      });
+
+      var quotient = d - oldest;
+      if (quotient == 0) return v;
+
+      // multiply to normalize => movement in pixels per millisecond
+      return v.multiplyScalar(1. / quotient);
+    }
+
+    this.startRecording = function () {
+      this.clear();
+      recording = true;
+    };
+
+    this.play = function () {
+      recording = false;
+
+      var epsilon = .03;
+      var friction = 1 - 1 / 25;
+      var acceleration = this.calculateMovement();
+      var lastMove = new Date();
+
+      var calcSpeed = function (vector) {
+        return Math.sqrt(vector.getX() * vector.getX() + vector.getY() * vector.getY());
+      };
+
+      var updateLocation = function (vector) {
+        var d = new Date();
+        var diff = d - lastMove;
+
+        moveImage(vector.getX() * diff, vector.getY() * diff);
+        lastMove = d;
+      };
+
+      var tick = function () {
+        if (calcSpeed(acceleration) > epsilon && !recording) setTimeout(tick, 5);
+        updateLocation(acceleration);
+        acceleration = acceleration.multiplyScalar(friction);
+      };
+      tick();
+
+      // TODO: play / accelerate image
+    };
+  }
+
   function PanoramaWidget() {
     var widget = this;
     var firstDrag = true;
@@ -203,6 +327,15 @@ $(document).ready(function () {
 
     var autoDrag = new AutoDrag(bigImage, thumbnail);
 
+    var accelerator = new Accelerator(function (x, y) {
+      bigImage.moveX(x);
+      bigImage.moveY(y);
+      thumbnail.updateRectangle(bigImage);
+    });
+    bigImage.onMove(function (vector) {
+      accelerator.recordMove(vector);
+    });
+
     this.isTouchEvent = function (event) {
       return event.originalEvent && event.originalEvent.touches && event.originalEvent.touches.length > 0;
     }
@@ -214,11 +347,6 @@ $(document).ready(function () {
     }
     this.convertSingleTouchEvent = function (event) {
       return event.originalEvent.touches[0];
-      /*if (event.originalEvent && event.originalEvent.touches && event.originalEvent.touches[0]) {
-       var touches = event.originalEvent.touches;
-       return touches.length == 1 ? touches[0] : null;
-       }
-       return event;*/
     }
 
     this.registerEvents = function () {
@@ -278,13 +406,16 @@ $(document).ready(function () {
         cropped.addClass("grabbing");
         firstDrag = true;
         autoDrag.stopAutoDrag();
+        accelerator.startRecording();
       });
 
       // could also use mouseout, then we wouldn't need to register the body events
       body.on("mouseup touchend", function (e) {
+        if (!cropped.hasClass("grabbing")) return;
         body.off("mousemove touchmove", manualDragBig);
         cropped.removeClass("grabbing");
         firstDrag = false;
+        accelerator.play();
       });
 
       rectangle.on("mousedown touchstart", function (e) {
@@ -292,13 +423,16 @@ $(document).ready(function () {
         rectangle.addClass("grabbing");
         firstDrag = true;
         autoDrag.stopAutoDrag();
+        accelerator.startRecording();
       });
 
       // could also use mouseout, then we wouldn't need to register the body events
       body.on("mouseup touchend", function (e) {
+        if (!rectangle.hasClass("grabbing")) return;
         body.off("mousemove touchmove", manualDragThumbnail);
         rectangle.removeClass("grabbing");
         firstDrag = false;
+        accelerator.play();
       });
 
       var clickFunction = function (offsetX) {
