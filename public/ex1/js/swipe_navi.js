3257304e674307cbb6d1502296bb796f99a854df
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-21 09:55:21 +0100

    swipe navi

--- public/ex1/js/swipe_navi.js
+++ public/ex1/js/swipe_navi.js
@@ -1,37 +1,46 @@
 "use strict";
 
 $(document).ready(function () {
+
   function Navigation() {
     var navigated = false;
+    var navElements = $("header nav ul li");
+
+    this.findCurrentIndex = function () {
+      for (var i = 0; i < navElements.length; ++i) {
+        if ($(navElements[i]).hasClass("active")) return i;
+      }
+      console.error("No active navigation element found!")
+    };
+
+    this.findElement = function (offset) {
+      return $(navElements[(this.findCurrentIndex() + offset + navElements.length) % navElements.length]).children("a")[0];
+    };
 
     this.next = function () {
-      if(navigated) return;
+      if (navigated) return;
       navigated = true;
-      // TODO: implement this
-    }
+      this.findElement(1).click();
+    };
 
     this.previous = function () {
-      if(navigated) return;
+      if (navigated) return;
       navigated = true;
-      // TODO: implement this
-    }
+      this.findElement(-1).click();
+    };
   }
 
   var navigation = new Navigation();
 
   $.touch.preventDefault = false;
-
-  $(document).ready(function () {
-    $('header').touchable({
-      gesture: function (e, touchHistory) {
-        if (touchHistory.match({ finger: 0, deltaX: '<-100', time: '1..100' })) {
-          navigation.next();
-        } else if (touchHistory.match({ finger: 0, deltaX: '>100', time: '1..100' })) {
-          navigation.previous();
-        }
+  $('header').touchable({
+    gesture: function (e, touchHistory) {
+      if (touchHistory.match({ finger: 0, deltaX: '<-100', time: '1..100' })) {
+        navigation.previous();
+      } else if (touchHistory.match({ finger: 0, deltaX: '>100', time: '1..100' })) {
+        navigation.next();
       }
-    });
+    }
   });
-
 
 });
