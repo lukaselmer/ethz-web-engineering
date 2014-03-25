8fb5ea63982bc977a932ce115cf47b22772e24c8
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-25 11:58:36 +0100

    random movement

--- public/ex1/js/script.js
+++ public/ex1/js/script.js
@@ -149,16 +149,19 @@ $(document).ready(function () {
     }
   }
 
-  function AutoDrag(_bigImage, _thumbnail) {
+  function AutoDrag(_bigImage, _thumbnail, _accelerator) {
     var _this = this;
 
     var bigImage = _bigImage;
     var thumbnail = _thumbnail;
+    var accelerator = _accelerator;
 
     var autoDragEnabled = true;
     var autoDragSpeed = 0.1;
 
     var marginOffset = 0, timeOffset = 0, lastTime = 0, manualDrag = true;
+
+    var randomEnabled = false, nextRandom = new Date();
 
     this.stopAutoDrag = function () {
       autoDragEnabled = false;
@@ -209,9 +212,18 @@ $(document).ready(function () {
     this.autoDrag = function (time) {
       if (!autoDragEnabled) return;
       var max = bigImage.maxMarginLeft();
-      var newPosition = this.calculateMarginLeft(time, max);
-      bigImage.moveTo("margin-left", newPosition, max);
-      thumbnail.updateRectangle(bigImage);
+      if (randomEnabled) {
+        if (nextRandom > new Date()) return;
+
+        nextRandom = new Date(+new Date() + 400. + 1000. * Math.random());
+        accelerator.startRecording();
+        accelerator.manualPushMove(new Vector(Math.random() - .5, Math.random() - .5).multiplyScalar(5.25 * Math.random()));
+        accelerator.play();
+      } else {
+        var newPosition = this.calculateMarginLeft(time, max);
+        bigImage.moveTo("margin-left", newPosition, max);
+        thumbnail.updateRectangle(bigImage);
+      }
     }
 
     this.play = function () {
@@ -228,6 +240,10 @@ $(document).ready(function () {
     this.decreaseSpeed = function () {
       this.setStartParameters();
       autoDragSpeed /= 1.5;
+    }
+
+    this.playRandom = function () {
+      randomEnabled = true;
     }
   }
 
@@ -263,6 +279,12 @@ $(document).ready(function () {
       if (records.length > maxRecordLength) records.shift();
     };
 
+    this.manualPushMove = function (movementVector) {
+      var d = new Date();
+      records.push(new Record(new Date(+d - 1000), new Vector(0., 0.)));
+      records.push(new Record(new Date(+d), movementVector));
+    }
+
     this.calculateMovement = function () {
       var d = new Date();
       var v = new Vector(0, 0);
@@ -277,6 +299,7 @@ $(document).ready(function () {
       });
 
       var quotient = d - oldest;
+
       if (quotient == 0) return v;
 
       // multiply to normalize => movement in pixels per millisecond
@@ -338,8 +361,6 @@ $(document).ready(function () {
     var rectangle = thumbnailDiv.children(".rectangle");
     var thumbnail = new Thumbnail(thumbnailDiv);
 
-    var autoDrag = new AutoDrag(bigImage, thumbnail);
-
     var lastZoomPoint = null, totalZoomFactor = 1.;
 
     var accelerator = new Accelerator(function (x, y) {
@@ -350,6 +371,8 @@ $(document).ready(function () {
     bigImage.onMove(function (vector) {
       accelerator.recordMove(vector);
     });
+
+    var autoDrag = new AutoDrag(bigImage, thumbnail, accelerator);
 
     this.isTouchEvent = function (event) {
       return event.originalEvent && event.originalEvent.touches && event.originalEvent.touches.length > 0;
@@ -405,8 +428,8 @@ $(document).ready(function () {
 
           var maxZoomFactor = 3., minZoomFactor = 0.9;
 
-          if(totalZoomFactor > maxZoomFactor) totalZoomFactor = maxZoomFactor;
-          if(totalZoomFactor < minZoomFactor) totalZoomFactor = minZoomFactor;
+          if (totalZoomFactor > maxZoomFactor) totalZoomFactor = maxZoomFactor;
+          if (totalZoomFactor < minZoomFactor) totalZoomFactor = minZoomFactor;
           bigImage.setZoomFactor(totalZoomFactor);
 
           lastZoomPoint = currentZoomPoint;
@@ -535,6 +558,12 @@ $(document).ready(function () {
       $("a[href=#slower]").on("click touchstart", function (e) {
         e.preventDefault();
         autoDrag.decreaseSpeed();
+      });
+
+      $("a[href=#random]").on("click touchstart", function (e) {
+        e.preventDefault();
+        bigImage.setZoomFactor(totalZoomFactor * 2.);
+        autoDrag.playRandom();
       });
     }
 
