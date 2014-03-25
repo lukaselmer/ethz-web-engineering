c7f74db1ae1a37854ea3ac465a6213e083b0b537
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-25 21:36:23 +0100

    Update generate_cache_manifest.rb

--- public/ex1/generate_cache_manifest.rb
+++ public/ex1/generate_cache_manifest.rb
@@ -1,14 +1,20 @@
 File.open('cache.appcache', 'w') do |f|
   f.puts "CACHE MANIFEST"
-  f.puts "# v = 1.0.0"
-  f.puts 'FALLBACK :'
-  f.puts '    / /index.html'
+  f.puts "# v = 1.0.1"
+  
+  f.puts
+  
+  f.puts 'CACHE:'
   f.puts 'index.html'
   f.puts 'reviewers.html'
   f.puts 'weekly.html'
+  
   %w(js css images).each do |folder|
     Dir.glob("#{folder}/**/*.*") do |file|
-      f.puts "#{file}"
+      f.puts "#{file}" unless file.ends_with? '.psd'
     end
   end
+  
+  f.puts 'FALLBACK :'
+  f.puts '    / /index.html'
 end
